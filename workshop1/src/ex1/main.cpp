11ffe7062880bf340d45e83a5faa801ab6c15d54
Georgii Zhulikov <l8doku@gmail.com>
2022-01-13 12:31:31 +0300

    Workshop 1 result 213-1

--- workshop1/src/ex1/main.cpp
+++ workshop1/src/ex1/main.cpp
@@ -2,21 +2,40 @@
 #include <iostream>
 #include <climits>
 
+
+
+int myFunction()
+{
+    using namespace std;
+    int a = 0;
+    cout << a;
+    return 1;
+}
 // the entrypoint of the application
 int main()
 {
+    using namespace std;
+    
     // put a string literal "Hello world!" to the standard output stream in order 
     // to print it at the console, which is normally associated with this stream
-    std::cout << "Hello world!";
-
-
+    cout << "Hello world!\n";
+    
+    float a;
+    a = 10.1;
+    std::cout << a << "\n";
+    
     size_t s1 = sizeof(char);
     size_t s2 = sizeof(short);
     size_t s3 = sizeof(int);
     size_t s4 = sizeof(long);
     size_t s5 = sizeof(long long);
 
-
+    
+    std::cout << s1 << "\n";
+    std::cout << s2 << "\n";
+    std::cout << s3 << "\n";
+    std::cout << s4 << "\n";
+    std::cout << s5 << "\n";
 
     // must return a "errorlevel code": 0 is for OK
     return 0;
